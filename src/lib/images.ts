9dfc40ab4ab35c43b0ecafb646e1418aff1fbc5c
Bernardo Lobo Marques <bernardolobomarques@gmail.com>
2025-08-24 05:33:43 +0000

    eu fiz algumas alterações no images.ts
    faça com que isso resolva e apar

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,9 +1,9 @@
 import type { BlogImage } from '@/types';
 
 export const availableImages: BlogImage[] = [
-  { key: 'mesa-pintura', name: 'Pintura em Aquarela na Mesa', path: '/mesa-pintura.png' },
-  { key: 'mapa', name: 'Mapa Mundi com Fios', path: '/mapa.png' },
-  { key: 'garota', name: 'Garota Lendo Livro', path: '/garota.png' },
+  { key: 'mesa-pintura', name: 'Pintura em Aquarela na Mesa', path: '/public/mesa-pintura.png' },
+  { key: 'mapa', name: 'Mapa Mundi com Fios', path: '/public/mapa.png' },
+  { key: 'garota', name: 'Garota Lendo Livro', path: '/public/garota.png' },
   { key: 'placeholder-1', name: 'Placeholder 1', path: 'https://placehold.co/800x600.png' },
 ];
 
